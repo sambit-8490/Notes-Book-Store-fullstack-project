a84db9e85dae94b5c0f462e75a3f665e6f5d3ff3
AvishkarMandlik <mandlikavi121@gmail.com>
2026-01-11 00:57:33 +0530

    email.js updated

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,9 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    service: 'gmail',
     auth: {
         user: process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASSWORD,
